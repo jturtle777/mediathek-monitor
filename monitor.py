dfeebc96aeab66867c189136bd06ac377683313a
jturtle777 <jgz7@gmx.net>
2026-08-16 21:55:21 +0200

    Change GET request to POST with JSON query
    Updated the monitor.py script to send a POST request with a JSON query to the MediathekViewWeb API instead of a GET request.

--- monitor.py
+++ monitor.py
@@ -8,14 +8,37 @@ print("Verbinde mit MediathekViewWeb...")
 
 url = "https://mediathekviewweb.de/api/query"
 
+# Eine ganz einfache Suchanfrage:
+# Wir suchen nach dem Begriff "Tatort".
+query = {
+    "queries": [
+        {
+            "fields": ["title"],
+            "query": "Tatort"
+        }
+    ],
+    "size": 5
+}
+
 try:
-    with urllib.request.urlopen(url, timeout=30) as response:
-        data = json.loads(response.read().decode("utf-8"))
+    data = json.dumps(query).encode("utf-8")
+
+    request = urllib.request.Request(
+        url,
+        data=data,
+        headers={
+            "Content-Type": "application/json"
+        },
+        method="POST"
+    )
+
+    with urllib.request.urlopen(request, timeout=30) as response:
+        result = json.loads(response.read().decode("utf-8"))
 
     print("Verbindung erfolgreich!")
     print()
     print("Antwort von MediathekViewWeb:")
-    print(type(data))
+    print(result)
 
 except Exception as e:
     print("FEHLER:")
